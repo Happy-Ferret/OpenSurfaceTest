61c3147e264b52576dd16f262ad8e42366260215
Happy-Ferret <MarkBauer@gmx.ch>
2018-07-25 21:28:19 +0200

    Initial commit

---
+++ chrome/content/main.js
@@ -0,0 +1,8 @@
+/* main.js - main.xul's codebehind.
+ *
+ * Copyright (c) 2016, Mark Bauermeister
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
---
+++ defaults/preferences/junior.js
@@ -0,0 +1,16 @@
+pref("toolkit.defaultChromeURI", "chrome://junior/content/panel.xul");
+pref("general.useragent.extra.junior", "junior/0.1");
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", false);
+pref("javascript.options.strict", false);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", false);
+pref("full-screen-api.enabled", true);
+/* Don't inherit OS locale */
+pref("intl.locale.matchOS", true);
+pref("dom.push.enable", true);
+pref("social.toast-notifications.enabled", true);
+pref("dom.webnotifications.enabled", true);
+pref("dom.webnotifications.serviceworker.enabled", true);
+/* Choose own fallback locale; later it can be overridden by the user */
+pref("devtools.debugger.remote-enabled", true);
